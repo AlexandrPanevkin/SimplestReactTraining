2dcf6d690b9c0f2d7f4df063f10c4acb02b5f265
AlexanderPanevkin <panevkin900@gmail.com>
2022-12-10 20:30:54 +0300

    add Banknotes, styledComponents

--- src/City.tsx
+++ src/City.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {CurrentBankomat} from "./CurrentBankomat";
 import {MoneyType} from "./App";
+import styled from "styled-components";
 
 type CityPropsType = {
-    data: any //встречаем денюжки
+    data: MoneyType[] //встречаем денюжки
 }
 
 export const City = (props: CityPropsType) => {
@@ -15,16 +16,30 @@ export const City = (props: CityPropsType) => {
     //         money={el}
     //     />
     // ))
-
+    const mappedMoney = props.data.map((el, index) => {
+        return (
+            <CurrentBankomat money={el}/>
+        )
+    })
 
     return (
         <div>
-            <div>Ну все парни, мапимся -выводим наши денюжки</div>
-            <div>На верстку ПОКА не обращаем внимания , сейчас занимаемся логикой</div>
-            {/*<div>{mappedMoney}</div>*/}
+            <Wrapper>
+                {mappedMoney}
+            </Wrapper>
+
         </div>
     );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  font-size: 30px;
+`
+
+
 //1
 // Вроде все норм, но нужно причесать код. Давайте создадим const mappedMoney = props.data.map(el=>el...)
 // Т.е. нам нужно вынести map из вертски, оставив в верстке только mappedMoney
--- src/CurrentBankomat.tsx
+++ src/CurrentBankomat.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
 import {MoneyType} from "./App";
+import styled from "styled-components";
 
 type CurrentBankomatPropsType = {
     money: MoneyType
 }
-export const CurrentBankomat = (props:CurrentBankomatPropsType) => {
+export const CurrentBankomat = (props: CurrentBankomatPropsType) => {
 
     return (
-        <div>
+        <Banknote color={props.money.banknotes==='Dollars' ? 'darkseagreen' : 'indianred'}>
             {props.money.banknotes}
-        </div>
+            <Value>{props.money.value}</Value>
+        </Banknote>
     );
 };
+const Banknote = styled.div`
+  background-color: ${props => {
+    if (props.color === 'indianred') {
+      return 'indianred'
+    } else {
+      return 'darkseagreen'
+    }
+  }
+  };
+  width: 400px;
+  height: 200px;
+  margin: 10px;
+`
+const BanknoteRed = styled.div`
+  background-color: indianred;
+  width: 400px;
+  height: 200px;
+  margin: 10px;
+`
+const BanknoteGreen = styled.div`
+  background-color: darkseagreen;
+  width: 400px;
+  height: 200px;
+  margin: 10px;
+`
+const Value = styled.div`
+  justify-content: center;
+`
